7d2b98a459663365841d8b744b9418366ed4690a
tta2yta <tta2yta@gmail.com>
2021-07-05 18:29:28 +0300

    Add fucton create_element

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ const bookArr = [
   { title: "Moving on", author: "Tedros" },
   { title: "Microverse", author: "Nkiruka" },
 ];
+const book_list_main = document.createElement("div");
+  book_list_main.className = "book_list_main";
 
 list(bookArr);
 
 function list(bookArr) {
-  let book_list_main = document.createElement("div");
-  book_list_main.className = "book_list_main";
+  
   bookArr.forEach((element, index) => {
     let book_list_sub = document.createElement("div");
     book_list_sub.id = index;
@@ -35,16 +36,33 @@ function list(bookArr) {
   document.getElementById("main").appendChild(book_list_main);
 }
 
-const addBook = document
-  .querySelector("#save")
-  .addEventListener("click", function () {
+const addBook = document .querySelector("#save").addEventListener("click", function () {
     const title1 = document.querySelector("#title").value;
     const author1 = document.querySelector("#author").value;
     if (title1 == "" && author1 == "") {
       alert("Please enter the title and author");
     } else {
       const book = { title: title1, author: author1 };
-      bookArr.push(book);
+     const last_index=bookArr.push(book);
       console.log(bookArr);
+
+   
     }
   });
+
+  const create_element= function(title, author, index){
+    let book_list_sub = document.createElement("div");
+    book_list_sub.id = index;
+    book_list_sub.className = "book_list_sub";
+    let book_title = document.createElement("div");
+    let book_author = document.createElement("div");
+    book_title.textContent = title;
+    book_author.textContent = author;
+    let btn_remove = document.createElement("button");
+    btn_remove.textContent = "Remove";
+    btn_remove.className = "btn_remove";
+    book_list_sub.appendChild(book_title);
+    book_list_sub.appendChild(book_author);
+    book_list_sub.appendChild(btn_remove);
+    book_list_main.appendChild(book_list_sub);
+  }
