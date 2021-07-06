a419bf3888e166a5c939dbcf35830bb9903b4314
enkog <nkirukaogu@gmail.com>
2021-07-06 02:21:49 +0100

    Refactor the button event and list function

--- js/script.js
+++ js/script.js
@@ -1,45 +1,48 @@
-const bookArr = [
-  { title: 'Moving on', author: 'Tedros' },
-  { title: 'Microverse', author: 'Nkiruka' },
-];
-const bookListMain = document.createElement('div');
-bookListMain.className = 'book_list_main';
+const btn = document.getElementById("save");
+const container = document.getElementById("container");
+
+let bookArr = [];
+
+btn.addEventListener("click", () => {
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+
+  const titleInput = title.value;
+  const authorInput = author.value;
+
+  const book = { title: titleInput, author: authorInput };
+  const bookArrSize = bookArr.push(book);
+
+  createElement(titleInput, authorInput, bookArrSize);
+});
+
+const bookListMain = document.createElement("div");
+bookListMain.className = "book_list_main";
 
 const createElement = function (title, author, index) {
-  const bookListSub = document.createElement('div');
+  const bookListSub = document.createElement("div");
   bookListSub.id = index;
-  bookListSub.className = 'book_list_sub';
-  const bookTtitle = document.createElement('div');
-  const bookAuthor = document.createElement('div');
-  bookTtitle.textContent = title;
+  bookListSub.className = "book_list_sub";
+  const bookTitle = document.createElement("div");
+  const bookAuthor = document.createElement("div");
+  bookTitle.textContent = title;
   bookAuthor.textContent = author;
-  const btnRemove = document.createElement('button');
-  btnRemove.textContent = 'Remove';
-  btnRemove.className = 'btn_remove';
-  bookListSub.appendChild(bookTtitle);
+  const btnRemove = document.createElement("button");
+  btnRemove.textContent = "Remove";
+  var btnId = "btn_remove" + index;
+  btnRemove.id = btnId;
+  btnRemove.className = "btn_remove";
+  bookListSub.appendChild(bookTitle);
   bookListSub.appendChild(bookAuthor);
   bookListSub.appendChild(btnRemove);
   bookListMain.appendChild(bookListSub);
 };
 
-function list(bookArr) {
-  bookArr.forEach((element, index) => {
+function list(arr) {
+  arr.forEach((element, index) => {
     createElement(element.title, element.author, index);
   });
-
-  document.getElementById('container').appendChild(bookListMain);
+  container.appendChild(bookListMain);
 }
 
 list(bookArr);
-
-document.querySelector('#save').addEventListener('click', () => {
-  const title1 = document.querySelector('#title').value;
-  const author1 = document.querySelector('#author').value;
-  if (title1 === '' && author1 === '') {
-    alert('Please enter the title and author');
-  } else {
-    const book = { title: title1, author: author1 };
-    const lastIndex = bookArr.push(book);
-    createElement(title1, author1, lastIndex);
-  }
-});
