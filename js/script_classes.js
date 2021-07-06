12c5ede78e3fb94f5e3b30f6ba4aaaa03528a848
tta2yta <tta2yta@gmail.com>
2021-07-06 12:30:07 +0300

    Add createElement method to clas

--- js/script_classes.js
+++ js/script_classes.js
@@ -4,6 +4,29 @@ class Book{
     }
 
       list(){
+        arrBook.forEach((element, index) => {
+            createElement(element.title, element.author, index);
+          });
+          container.appendChild(bookListMain);
+    }
 
+    createElement (title, author, index){
+        const btnId = `btn_remove${index}`;
+        const bookListSub = document.createElement('div');
+        bookListSub.id = index;
+        bookListSub.className = 'book_list_sub';
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        bookTitle.textContent = title;
+        bookAuthor.textContent = author;
+        const btnRemove = document.createElement('button');
+        btnRemove.textContent = 'Remove';
+      
+        btnRemove.id = btnId;
+        btnRemove.className = 'btn_remove';
+        bookListSub.appendChild(bookTitle);
+        bookListSub.appendChild(bookAuthor);
+        bookListSub.appendChild(btnRemove);
+        bookListMain.appendChild(bookListSub);
     }
 }
