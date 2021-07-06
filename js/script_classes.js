d8bcc498dba39ced8745babf774047069f70f1e5
tta2yta <tta2yta@gmail.com>
2021-07-06 17:11:33 +0300

    Fix linters

--- js/script_classes.js
+++ js/script_classes.js
@@ -1,13 +1,18 @@
+const container = document.getElementById('container');
+const bookListMain = document.createElement('div');
+bookListMain.className = 'book_list_main';
+bookListMain.id = 'book_list_main';
+
 class Book {
-  constructor(author = null, title = null) {
+  constructor() {
     this.arrBook = [{ author: 'aaa', title: 'book1' }];
   }
 
-  getBooks(){
-      this.arrBook=JSON.parse(localStorage.getItem('Books'))
+  getBooks() {
+    this.arrBook = JSON.parse(localStorage.getItem('Books'));
   }
 
-  list(container) {
+  list() {
     this.arrBook.forEach((element, index) => {
       this.createElement(element.title, element.author, index);
     });
@@ -36,38 +41,27 @@ class Book {
   }
 
   addBook(title, author) {
-    const book = { title: title, author: author };
-    const bookArrSize = this.arrBook.push(book);  
+    const book = { title, author };
+    const bookArrSize = this.arrBook.push(book);
     this.createElement(title, author, bookArrSize);
     localStorage.setItem('Books', JSON.stringify(this.arrBook));
   }
 
-  removeBook(index){
+  removeBook(index) {
     const books = this.arrBook.filter((book, i) => i !== index);
     localStorage.setItem('Books', JSON.stringify(books));
     const parent = document.getElementById(index);
     parent.parentNode.removeChild(parent);
-    
   }
 }
 
-let myBook = new Book();
-myBook.getBooks()
-
-const bookListMain = document.createElement('div');
-bookListMain.className = 'book_list_main';
-bookListMain.className = 'book_list_main';
+const myBook = new Book();
+myBook.getBooks();
 
 const btn = document.getElementById('save');
-const container = document.getElementById('container');
-
-myBook.list(container);
-btn.addEventListener('click', ()=>{
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    if(title==='' || author==='')
-    alert("Please Provide value for Author and Tiltle")
-    else
-    myBook.addBook(title, author)
+myBook.list();
+btn.addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  if (title === '' || author === '') { alert('Please Provide value for Author and Tiltle'); } else { myBook.addBook(title, author); }
 });
-
