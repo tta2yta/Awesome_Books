312cf8fe2bd27643d8c44d562d4fb72d8136ea7d
enkog <nkirukaogu@gmail.com>
2021-07-06 16:19:30 +0100

    Remove alert

--- js/script_classes.js
+++ js/script_classes.js
@@ -68,9 +68,6 @@ myBook.list();
 btn.addEventListener('click', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-  if (title === '' || author === '') {
-    alert('Please Provide value for Author and Tiltle');
-  } else {
-    myBook.addBook(title, author);
-  }
+
+  myBook.addBook(title, author);
 });
